f125070b262f367760135822fb247b925315858c
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2021-03-31 14:59:27 -0500

    Update confbot.sh

--- confbot.sh
+++ confbot.sh
@@ -10,20 +10,20 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 echo "$IP" > /usr/bin/vendor_code
 }
 function_verify () {
-#  permited=$(curl -sSL "https://www.dropbox.com/s/8918s8z3py6jq7r/Control-IP")
-#  [[ $(echo $permited|grep "${IP}") = "" ]] && {
-#  clear
-#  echo -e "\n\n\n\e[31m====================================================="
-#  echo -e "\e[31m      ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!\n     SI DESEAS USAR EL BOTGEN CONTACTE A @Rufu99"
-#  echo -e "\e[31m=====================================================\n\n\n\e[0m"
-#  [[ -d /etc/ADM-db ]] && rm -rf /etc/ADM-db
-#[[ ! -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
-#  exit 1
-#  } || {
-  ### INTALAR VERCION DE SCRIPT
+ permited=$(curl -sSL "https://raw.githubusercontent.com/ChumoGH/VPSbot/main/TeleBotGen/Control/Control-Bot")
+  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+  clear
+  echo -e "\n\n\n\e[31m====================================================="
+  echo -e "\e[31m      ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!\n     SI DESEAS USAR EL BOTGEN CONTACTE A @ChumoGH"
+  echo -e "\e[31m=====================================================\n\n\n\e[0m"
+  [[ -d /etc/ADM-db ]] && rm -rf /etc/ADM-db
+[[ ! -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
+  exit 1
+  } || {
+ ### INTALAR VERCION DE SCRIPT
   v1=$(curl -sSL "https://www.dropbox.com/s/blxo0jifysvyrey/v-new.log")
   echo "$v1" > /etc/ADM-db/vercion
-#  }
+  }
 }
 
 veryfy_fun () {
